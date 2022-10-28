982158836690c703e29faa884653285f0182cff9
amy29 <dth.an292@gmail.com>
2022-10-28 23:03:06 +0700

    Api base

---
+++ src/constant/api_constant.jsx
@@ -0,0 +1,4 @@
+export const API_URL = 'https://5aed-113-185-77-93.ngrok.io'
+export const ACCESS_TOKEN = localStorage.getItem('accessToken')
+
+export const setToken = (token) => localStorage.setItem('accessToken', token);
